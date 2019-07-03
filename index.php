db1c0a7ab622e6846c0692933eafbb8e8e448fe8
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2019-07-02 21:29:44 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,233 @@
+<html><head>
+	<title>Alex Abdulla</title>
+	  	<meta charset="UTF-8">
+		<meta name="description" content="Personal Website">
+		<meta name="keywords" content="alex abdulla">
+		<meta name="author" content="alex abdulla">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0;">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link href="style.css" rel="stylesheet">
+	    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+		<link href="http://fonts.googleapis.com/css?family=Rokkitt|Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet" type="text/css"> 
+		<link href="http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic" rel="stylesheet" type="text/css">   
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.8/magnific-popup.min.css" rel="stylesheet">
+	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/0.9.8/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script><script async="" src="//www.google-analytics.com/analytics.js"></script><script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			
+			ga('create', 'UA-41348693-1', 'alexabdulla.com');
+			ga('send', 'pageview');
+		</script> 
+	</head>
+	<body>
+		<!-- Navigation 
+		<div id="toc-holder" class="toc-holder">
+			<a href="#" class="toc-link" id="toc-link"><span class="">☰</span></a>
+			<ul id="toc" class="toc toc-up" style="top: -310px;">
+				<li class="toc-h1"><a href="#about">About</a></li>
+				<li class="toc-h1"><a href="#resume">Résumé</a></li>
+				<li class="toc-h1"><a href="#work">Work</a></li>
+			</ul>
+		</div>
+        -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+  <a class="navbar-brand" href="#">Alexander Abdulla</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/AlexAbdullaWebsite/#about">About<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/AlexAbdullaWebsite/#resume">Resume</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/AlexAbdullaWebsite/#work">Projects</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+		<div id="page-wrapper">
+			<!-- Header 
+			<div id="header">
+				<div class="inner name"><a href="#illustration">Alex Abdulla</a></div>
+				<div class="nav">
+				<ul class="mobile-nav">
+				<li class="mobile-header"><a href="#about">About</a></li>
+				<li class="mobile-header"><a href="#resume">Resumé</a></li>
+				<li class="mobile-header"><a href="#work">Work</a></li></ul>
+				</div>
+			</div>-->
+			<!-- Essentials Illustration -->
+			<div id="illustration">
+                <center>
+                    <img id="laptop" src="img/laptopsvg.svg">
+                </center>
+            </div>
+			<!-- About Me -->
+			<div id="about"><div class="inner">
+				<p>Hello, world!<br> I'm a full stack web developer!</p><br><br><br>
+				<!-- Console -->
+					<div id="code">
+						<div id="console">
+							&gt; Alexander.origin<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "British Columbia, Canada"<br><br></span>
+							&gt; Alexander.education<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "British Columbia Instititute of Technology"<br><br></span>
+							&gt; Alexander.major<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "Computer Systems Technology"<br><br></span>
+                            &gt; Alexander.specialization<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "Web and Mobile Development"<br><br></span>
+                            &gt; Alexander.graduation<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "December 2018"<br><br></span>
+                            &gt; Alexander.interests<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; ["Design", "Code", "Food", "Languages", "History", "Creative Writing", "Hiking"]<br><br></span>
+							&gt; Alexander.workExperience.latest<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; Latest Work Experience<br>
+												 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; company: "Linux Magic"<br>
+												 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; role: "PHP Developer and Product Manager"<br>
+												 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; startDate: "January 2018"<br>
+							<br></span>
+							&gt; Alexander.resume<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "<a target="_blank" href="AlexAbdullaResume.pdf">AlexAbdullaResume.pdf</a>"<br><br></span>
+							&gt; Alexander.contact<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "<a target="_blank" href="mailto:alexander.amadeus.abdulla@gmail.com">alexander.amadeus.abdulla@gmail.com</a>"<br><br></span>
+                        	&gt; Alexander.linkedIn<br>
+							<span class="answer">&nbsp;&nbsp;=&gt; "<a target="_blank" href="https://www.linkedin.com/in/alexander-abdulla-b5b314163/">linkedin.com/in/alexander-abdulla</a>"<br><br></span>
+                        </div>
+					</div>
+				</div>
+				<br>
+			</div>
+			<!-- Résumé -->
+			<div id="resume"><div class="inner">
+				<p class="tagline">Résumé</p>
+				<!-- Résumé : Education -->
+				<p class="section-tagline" style="text-align:left;">Education</p>
+				<!-- Résumé : Education : BCIT -->
+				<p>
+					<a target="_blank" href="https://www.bcit.ca/"><img class="icon" src="img/bcit.png"></a><b>British Columbia Institute of Technology</b>
+					<span class="right"><i>2016 - 2018</i></span><br><b>Computer Systems Technology, Diploma</b>
+					<span class="right"><i>Burnaby, B.C.</i></span><br><br>
+						Relevant Coursework:<br>
+						</p><ul style="list-style:circle;">
+									<li>Object-Oriented Programming</li>
+									<li>Principles of Software Development</li>
+									<li>Discrete Methods in Computer Science</li> 
+									<li>Design and Analysis of Algorithms</li>
+									<li>Web and Mobile Design and Application</li>
+									<li>Operating Systems</li>
+									<li>File and Database Management</li>
+									<li>Project Management and Client Relations</li>
+									<li>Android Studio / XCode / ASP.NET Core / Angular </li>								
+								</ul>
+					<span class="skill">C/C++</span><span class="skill">Java</span><span class="skill">SQL</span><span class="skill">Git</span>
+					<span class="skill">PHP</span><span class="skill">JavaScript / TypeScript</span><span class="skill">C#</span>
+				<p></p>
+				<br>
+				<!-- Résumé : Education : UBC -->
+				<p>
+					<a target="_blank" href="https://www.ubc.ca"/><img class="icon" src="img/ubc.jpg"></a><b>University of British Columbia</b>
+					<span class="right"><i>2009-2013</i></span><br>
+					<b>Bachelor of Philosophy</b>
+					<span class="right"><i>Vancouver, B.C.</i></span><br>
+								<ul>
+									<li>Final Thesis on “The Cognitive Revolution” (98%)</li>
+									<li>Symbolic Logic / History of Computer Science </li>							
+								</ul>
+
+					<span class="skill">Academic Writing</span><span class="skill">Classroom Presentation</span><span class="skill">Research and Analysis</span>
+				</p>
+				<br><br>
+
+				<!-- Résumé : Work Experience -->
+				<p class="section-tagline" style="text-align:left;">Work Experience</p>
+
+				<!-- Résumé : Work Experience : Linux Magic -->
+				<p>
+					<a target="_blank" href="http://www.linuxmagic.com"><img class="icon" src="img/linuxmagic.png"></a><b>Linux Magic</b>
+					<span class="right"><i>January 2018</i></span><br>
+					<b>Software Developer</b>
+					<span class="right"><i>Vancouver, B.C.</i></span><br><br>
+					My primary role is as a full stack PHP developer maintaining our codebase as well as ocassionally spearheading new features. This role involves OOP native development with PHP / HTML / CSS / JS / JQuery. <br><br>
+					My secondary role is as Junior Product Manager in training for MagicMail. This role entails scoping out new features and delegating to our development team.<br><br>
+					<span class="skill">HTML</span><span class="skill">CSS</span><span class="skill">JS</span><span class="skill">PHP</span><span class="skill">SVN</span><span class="skill">Apache</span><span class="skill design">PSQL</span><span class="skill design">JQuery</span>
+				</p>
+				<br>
+				<!-- Résumé : Work Experience : EPIK -->
+				<p>
+					<a target="_blank" href="https://www.epik.go.kr"><img class="icon" src="img/epik.jpg"></a><b>English Program in Korea</b>
+					<span class="right"><i>2014 - 2016</i></span><br>
+					<b>ESL Teacher</b>
+					<span class="right"><i>Suncheon, South Korea</i></span><br><br>
+						I have been employed as an ESL teacher in a variety of roles since 2013 both in Canada and Korea. EPIK was my first and defining role as a Native English Teacher. From there
+						I have gone on to teach at several institutions locally as well. I credit this job with giving me the confidence and soft skills to move to a new career in Software Development.<br><br>
+					<span class="skill">Project Management</span><span class="skill">Communication / Language Skill</span><span class="skill">Planning / Documentation</span>
+				</p>
+				<br><br><br><br>
+			</div></div>
+			<!-- Portfolio -->
+			<div id="work"><div class="inner">
+				<p class="tagline">Projects</p>
+				<h3>This section is under constrcution!<br><br> For now please check my github:<br><br> <a href=https://github.com/AlexanderAbdulla>LINK!</a></h3>				
+				<!--<div class="portfolio-thumb portfolio-big">
+					<a class="image-popup-no-margins" href="img/portfolio/happen.png"><img src="img/portfolio/happen.png"></a>
+					<p class="description"><b>Happen</b>&nbsp;&nbsp; A group project for our senior software design capstone class inspired by our collective frustration with current social media’s focus on making our backlogs public. We want to create a tool for people to make plans for the future with all interactions between users kept private. This will be a simple planning platform where users can keep a list of things they want to do and friends can ambiently express their interest by swiping “me too.” We expect further communication to take place outside the app and are interested in the simplicity of its humble feature set, and its potential to encourage offline communication and activity. Working as a designer and developer. Currently in development. 
+
+</p>
+				</div>
+				<div class="row">
+					<div class="portfolio-thumb">
+						<a class="image-popup-no-margins" href="img/portfolio/bike2.png"><img src="img/portfolio/bike2.png"></a>
+						<p class="description"><b>Illustrations</b>&nbsp;&nbsp; I like to draw things I like. My Schwinn ride pictured above.</p>
+					</div>
+					<div class="portfolio-thumb">
+						<a class="image-popup-no-margins" href="img/portfolio/labs2.png"><img src="img/portfolio/labs2.png"></a>
+						<p class="description"><b>IDEO Labs</b>&nbsp;&nbsp; Worked on front-end development of a wordpress theme designed for the new <a target="_blank" href="http://labs.ideo.com">IDEO Labs site</a>.</p>
+					</div>
+				</div>						
+				<div class="row">
+					<div class="portfolio-thumb">
+						<a class="image-popup-no-margins" href="img/portfolio/apollo2.png"><img src="img/portfolio/apollo2.png"></a>
+						<p class="description"><b>Apollo</b>&nbsp;&nbsp; An internal summer intern project at IDEO. Apollo is
+						 a searchable community of people tagged with rich psychographic data, curated to provide resolution and reach to companies looking
+						 for research candidates.
+						</p>
+					</div>
+					<div class="portfolio-thumb">
+						<a class="image-popup-no-margins" href="img/portfolio/xx_03.png"><img src="img/portfolio/xx_03.png"></a>
+						<p class="description"><b>Music Visualizer</b>&nbsp;&nbsp; Experimental summer
+						project using WebGL and Three.js with a beat detection algorithm to make an interactive 3D music visualization of album artwork using orbit controls. Play <a target="_blank" href="visualizer/xx.html">here</a>.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="portfolio-thumb">
+						<a class="popup-vimeo" href="http://vimeo.com/41632558"><img src="img/portfolio/cube2.png"></a>
+						<p class="description"><b>LED Cube</b>&nbsp;&nbsp; Built with a friend as a final project for USC EE 201: Introduction to Digital Circuits. Constructed an 8x8x8 LED Cube three-dimensional display. Designed and wired the driver circuit board. Wrote application modules in Verilog to navigate the cube. </p>
+					</div>
+					<div class="portfolio-thumb">
+						<a class="image-popup-no-margins" href="img/portfolio/homefit.png"><img src="img/portfolio/homefit.png"></a>
+						<p class="description"><b>Homefit</b>&nbsp;&nbsp; Working with Executive Director of Digital Innovation at USC Viterbi School of Engineering, Ashish Soni, to research, conceptualize, wireframe, and design a new online tool for people looking to buy a home. </p></div>
+					</div>
+				</div>-->
+			</div></div>
+			<div id="footer"><div class="inner">
+				© 2019 Alex Abdulla. 
+				<span class="right">
+					<a target="_blank" href="mailto:alexander.amadeus.abdulla@gmail.com">Email</a>&nbsp;//&nbsp;
+					<a target="_blank" href="https://www.linkedin.com/in/alexander-abdulla-b5b314163/">LinkedIn</a>&nbsp;//&nbsp;
+				</span>
+			</div></div>
+	
+</body></html>
